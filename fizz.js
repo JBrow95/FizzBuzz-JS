00d7a33fb8eddbc51b6381996a6f371301c4ab7a
Jbrown95 <browningjustin95@gmail.com>
2018-12-10 00:23:04 -0500

    same for 5, printed fizz for every mulitple of 5

--- fizz.js
+++ fizz.js
@@ -2,8 +2,10 @@ for (var f = 1; f < 101; f++) {
     if (f % 15 == 0) {
         console.log("FizzBuzz")
     }
+    else if (f % 5 == 0) {
+        console.log("Fizz")
+    }
     else {
         console.log(f)
     };
-   
-}
+};
