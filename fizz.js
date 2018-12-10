969fc4fe7bd5d42eb79f92463fd88e96051dae78
Jbrown95 <browningjustin95@gmail.com>
2018-12-10 00:24:19 -0500

    same for 3, console logged buzz for every multiple of 3

--- fizz.js
+++ fizz.js
@@ -1,9 +1,12 @@
 for (var f = 1; f < 101; f++) {
     if (f % 15 == 0) {
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
     }
     else if (f % 5 == 0) {
-        console.log("Fizz")
+        console.log("Fizz");
+    }
+    else if (f % 3 == 0) {
+        console.log("Buzz");
     }
     else {
         console.log(f)
